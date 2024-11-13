54d19c9c2aa5e84d2801bb347553f97332a1906d
shinmink <sinmk99@hansung.ac.kr>
2024-11-13 16:29:10 +0900

    FEAT : toggleTheme 추가로 라이트, 다크 모드 추가 예정 FIX : 챗 컨테이너 위치 미세 조정

--- src/components/Home/Footer/Footer.jsx
+++ src/components/Home/Footer/Footer.jsx
@@ -3,15 +3,21 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // Link 컴포넌트 임포트
 import './Footer.css'; // 스타일 시트를 별도로 임포트
 
-const Footer = () => {
+const Footer = ({ theme, toggleTheme }) => {
     return (
         <footer className="footer">
-            © HansungCampusLink |{' '}
-            <Link to="/team-info" className="team-info-link">Contact Us</Link> |{' '}
-            <a href="https://www.hansung.ac.kr" target="_blank" rel="noopener noreferrer" className="hansung-link">
-                Hansung University
-            </a>
+            <div className="footer-content">
+                © HansungCampusLink |{' '}
+                <Link to="/team-info" className="team-info-link">Contact Us</Link> |{' '}
+                <a href="https://www.hansung.ac.kr" target="_blank" rel="noopener noreferrer" className="hansung-link">
+                    Hansung University
+                </a>
+            </div>
+                <div className="toggle-switch" onClick={toggleTheme}> {/* 토글 스위치 */}
+                    <div className={`toggle-circle ${theme === 'dark' ? 'toggle-dark' : 'toggle-light'}`}></div>
+                </div>
         </footer>
+
     );
 };
 
