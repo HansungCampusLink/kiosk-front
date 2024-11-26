938a47c12ceb8b616ea0a5b6afabe2fb7f231e30
shinmink <sinmk99@hansung.ac.kr>
2024-11-26 17:00:18 +0900

    FEAT : 팀원 깃헙 주소 입력

--- src/components/Home/Footer/TeamInfoPage.jsx
+++ src/components/Home/Footer/TeamInfoPage.jsx
@@ -4,11 +4,11 @@ import './TeamInfoPage.css';
 
 const TeamInfoPage = () => {
     const teamMembers = [
-        { name: '이경미', role: '팀장 / 데이터베이스 연동 및 API 개발', bio: '깃헙 주소.' },
-        { name: '박정제', role: '백엔드, 프롬프트 엔지니어', bio: '깃헙 주소.' },
-        { name: '민경빈', role: 'AI 엔지니어', bio: '깃헙 주소.' },
-        { name: '유호준', role: '데이터 엔지니어', bio: '깃헙 주소' },
-        { name: '신민금', role: '프론트엔드 개발자', bio: '깃헙 주소' },
+        { name: '이경미', role: '팀장 / 데이터베이스 연동 및 API 개발', bio: 'https://github.com/LEEKYUNGIMI' },
+        { name: '박정제', role: '백엔드, 프롬프트 엔지니어', bio: 'https://github.com/LuizyHub' },
+        { name: '민경빈', role: 'AI 엔지니어', bio: 'https://github.com/walesmin' },
+        { name: '유호준', role: '데이터 엔지니어', bio: 'https://github.com/wns5120' },
+        { name: '신민금', role: '프론트엔드 개발자', bio: 'https://github.com/shinmink' },
     ];
 
     return (
@@ -20,7 +20,14 @@ const TeamInfoPage = () => {
                     <div key={index} className="team-member-card">
                         <h2>{member.name}</h2>
                         <p className="role">{member.role}</p>
-                        <p className="bio">{member.bio}</p>
+                        <p
+                            className="bio"
+                            onClick={() => window.location.href = member.link}
+                            style={{cursor: 'pointer', color: 'blue', textDecoration: 'underline'}}
+                        >
+                            {member.bio}
+                        </p>
+
                     </div>
                 ))}
             </div>
